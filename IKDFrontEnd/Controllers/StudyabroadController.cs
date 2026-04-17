58072ee7222d1a807a41323103e2ad2fc5488457
Asif Tufail <asiftufail321@hotmail.com>
2026-04-17 14:34:22 +0500

    asif

--- IKDFrontEnd/Controllers/StudyabroadController.cs
+++ IKDFrontEnd/Controllers/StudyabroadController.cs
@@ -51,73 +51,133 @@ namespace IKDFrontEnd.Controllers
             return View("Home", guideDetail);
         }
 
-        [HttpGet("studyabroad/{detailSlug}")]
-        public async Task<IActionResult> Detail(string detailSlug)
-     {
-            var sectionContent = await _contextCollege.TblAllGuidesCms
-                      .Where(c => c.Url == "/studyabroad/" + detailSlug)
-                      .FirstOrDefaultAsync();
+		[HttpGet("studyabroad/{detailSlug}")]
+		public async Task<IActionResult> Detail(string detailSlug)
+		{
+			// 1. Find SectionTypeImport by URL
+			var sectionType = await _contextCollege.SectionTypeImports
+				.FirstOrDefaultAsync(x => x.Url.Contains(detailSlug));
+			if (sectionType == null)
+			{
+				return NotFound();
+			}
+			// 2. Use its Id to fetch SectionContentImport
+			var sectionContent = await _contextCollege.SectionContentImports
+				.FirstOrDefaultAsync(x => x.ContentId == sectionType.Id);
+
 			if (sectionContent == null)
-            {
-                var slug = detailSlug.Split("-")[0];
-                var guideUrl = $"https://www.ilmkidunya.com/{slug}";
-                
-                var guide = await _contextCollege.TblGuidesDefinations.FirstOrDefaultAsync(g => g.GuideMainUrl == guideUrl || g.GuideMainUrl == guideUrl + '/');
-                if(guide != null)
-                {
-                    string redirectUrl = $"/{slug}/{detailSlug}";
-                    return Redirect(redirectUrl);
-                }
-                return NotFound();
-            }
+			{
+				return NotFound();
+			}
 
-            var guideDetail = new GuideDetailViewModel
-            {
-                
-                Heading = sectionContent?.Heading,
-                HeadingDesc = sectionContent?.MetaDesc,
-                MetaTitle = sectionContent?.MetaTitle,
-                MetaDesc = sectionContent?.MetaDesc,
-                MetaKeyword = sectionContent?.MetaKeys,
-                Url = sectionContent?.Url,
-                Detail = sectionContent?.Desc1,
-                Detail2 = sectionContent?.Desc2
-            };
+			// 3. Build view model
+			var guideDetail = new GuideDetailViewModel
+			{
+				Heading = sectionContent.Heading,
+				HeadingDesc = sectionContent.MetaDesc,
+				MetaTitle = sectionContent.MetaTitle,
+				MetaDesc = sectionContent.MetaDesc,
+				MetaKeyword = sectionContent.MetaKeyword,
+				Url = sectionType.Url,
+				Detail = sectionContent.Detail,
+				Detail2 = sectionContent.Detail2
+			};
+			// 4. Country logic (unchanged but safer)
+			var countryUrl = detailSlug
+				.Replace("study-abroad-in-", "")
+				.Replace(".aspx", "")
+				.Split('-')[0];
 
-           
-             var   url = detailSlug.Replace("study-abroad-in-", "")
-                     .Replace(".aspx", "");
-            var countryUrl = url.Split('-')[0];
-            var country = _context.TblDefCountries
-                   .Where(c => c.Url == countryUrl)
-                   .Select(c => new {
-                       c.CountryName,
-                       c.Url,
-                       c.ImageUrl
-                   })
-                   .FirstOrDefault();
+			var country = await _context.TblDefCountries
+				.Where(c => c.Url == countryUrl)
+				.Select(c => new
+				{
+					c.CountryName,
+					c.Url,
+					c.ImageUrl
+				})
+				.FirstOrDefaultAsync();
 
-            if (country != null)
-            {
-                ViewBag.Country = country;
-            }
-            else
-            {
-                ViewBag.Country = new
-                {
-                    CountryName = "Not Available",
-                    Url = "Not Available",
-                    ImageUrl = "/images/not-available.png"
-                };
-            }
+			ViewBag.Country = country ?? new
+			{
+				CountryName = "Not Available",
+				Url = "Not Available",
+				ImageUrl = "/images/not-available.png"
+			};
 
-            var banners = await _bannerService.GetBannersAsync();
-            ViewBag.Banners = banners;
-            return View("Detail", guideDetail);
-        }
+			ViewBag.Banners = await _bannerService.GetBannersAsync();
+
+			return View("Detail", guideDetail);
+			
+		}
+
+		//     [HttpGet("studyabroad/{detailSlug}")]
+		//     public async Task<IActionResult> Detail(string detailSlug)
+		//     {
+		//var sectionContent = await _contextCollege.TblAllGuidesCms
+		//		  .Where(c => c.Url == "/studyabroad/" + detailSlug)
+		//		  .FirstOrDefaultAsync();
+		//if (sectionContent == null)
+		//         {
+		//             var slug = detailSlug.Split("-")[0];
+		//             var guideUrl = $"https://www.ilmkidunya.com/{slug}";
+
+		//	var guide = await _contextCollege.TblGuidesDefinations.FirstOrDefaultAsync(g => g.GuideMainUrl == guideUrl || g.GuideMainUrl == guideUrl + '/');
+		//	if (guide != null)
+		//             {
+		//                 string redirectUrl = $"/{slug}/{detailSlug}";
+		//                 return Redirect(redirectUrl);
+		//             }
+		//             return NotFound();
+		//         }
+
+		//         var guideDetail = new GuideDetailViewModel
+		//         {
+
+		//             Heading = sectionContent?.Heading,
+		//             HeadingDesc = sectionContent?.MetaDesc,
+		//             MetaTitle = sectionContent?.MetaTitle,
+		//             MetaDesc = sectionContent?.MetaDesc,
+		//             MetaKeyword = sectionContent?.MetaKeys,
+		//             Url = sectionContent?.Url,
+		//             Detail = sectionContent?.Desc1,
+		//             Detail2 = sectionContent?.Desc2
+		//         };
 
 
-        [Route("studyabroad/country-guides.aspx")]
+		//          var   url = detailSlug.Replace("study-abroad-in-", "")
+		//                  .Replace(".aspx", "");
+		//         var countryUrl = url.Split('-')[0];
+		//         var country = _context.TblDefCountries
+		//                .Where(c => c.Url == countryUrl)
+		//                .Select(c => new {
+		//                    c.CountryName,
+		//                    c.Url,
+		//                    c.ImageUrl
+		//                })
+		//                .FirstOrDefault();
+
+		//         if (country != null)
+		//         {
+		//             ViewBag.Country = country;
+		//         }
+		//         else
+		//         {
+		//             ViewBag.Country = new
+		//             {
+		//                 CountryName = "Not Available",
+		//                 Url = "Not Available",
+		//                 ImageUrl = "/images/not-available.png"
+		//             };
+		//         }
+
+		//         var banners = await _bannerService.GetBannersAsync();
+		//         ViewBag.Banners = banners;
+		//         return View("Detail", guideDetail);
+		//     }
+
+
+		[Route("studyabroad/country-guides.aspx")]
         public async Task<IActionResult> CountryGuideAllCountries(string guideSlug)
         {
 
